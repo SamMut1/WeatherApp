66f3f734c49b2ebfc9dd6ee369249109ff98e048
sammut1 <samuel.mutandiro@gmail.com>
2022-06-15 15:47:21 +0100

    working can see the end

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import Moment from 'react-moment'
 
 function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
 // & units=metric = How to change unit of mesurement
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=895284fb2d2c50a520ea537456963d9c`
+
+  const minTemp = props => <p>{data.main.daily.temp.min}</p>
 
   // set Location function
   const searchLocation = (event) => {
@@ -28,23 +31,35 @@ function App() {
           placeholder='Enter Location'
           type="text" />
       </div>
-      <div className="container">
+      <div className="container1">
         <div className="top">
           <div className="location">
             <p>{data.name}</p>
+            <p><Moment format="DD-MM-yyyy">{data.date}</Moment></p>
           </div>
           <div className="temp">
-            {data.main ? <h1>{data.main.temp.toFixed()}°D</h1> : null}
+            {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
           </div>
           <div className="description">
             {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
 
-        {data.name !== undefined &&
+       <container className="forecast">
+       <p><Moment format="DD-MM-yyyy">{data.date}</Moment></p>
+       <p>{data.name}</p>
+       <p>{data.main.temp.toFixed()} *C</p>
+     
+       
+        
+
+       </container>
+
+          {data.name !== undefined && //hides the data if no location is entered.
+        <container>
           <div className="bottom">
             <div className="feels">
-              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°D</p> : null}
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°C</p> : null}
               <p>Feels Like</p>
             </div>
             <div className="humidity">
@@ -55,14 +70,41 @@ function App() {
               {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
               <p>Wind Speed</p>
             </div>
+            <br></br>
           </div>
+           <div className="bottom1">
+             
+             <div><p>Min Temp</p>
+              {data.main ? <p className='bold'>{data.main.temp_min.toFixed()}°C </p> : null}
+              </div>
+              <div className="max_temp">
+              <p>Max Temp</p>
+              {data.main ? <p className='bold'>{data.main.temp_max.toFixed()} °C</p> : null}
+              
+               </div>
+             </div>
+            </container>
         }
-
-
-
       </div>
-    </div>
+      </div>
   );
 }
 
 export default App;
+{/*
+
+        <div className="bottom1">
+            <div className="temp_min">
+          {data.main ? <p>{data.main.temp_min.toFixed()}°C </p> : null} 
+          <p>Min Temp</p>
+            <div className="max_temp">
+              {data.data ? <p className='bold'>{data.main.temp_max.toFixed()} °C</p> : null}
+              <p>Max Temp</p>
+            </div>
+          </div>
+        
+      </div>
+    </div>
+
+
+ */}
